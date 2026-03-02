2f2b667ee905e75f5c10407fac94c827d8fd1b83
sak3-9 <t.jackjam@gmail.com>
2026-03-02 16:13:51 +0900

    Delete task directory

--- task/supabase/migrations/005_remove_workspace_members_rpc.sql
+++
@@ -1,5 +0,0 @@
--- 005_remove_workspace_members_rpc.sql
--- UI now uses SELECT on public.profiles (id, display_name) under RLS.
--- Remove unused RPC to reduce exposed DB surface.
-
-drop function if exists public.workspace_members();
